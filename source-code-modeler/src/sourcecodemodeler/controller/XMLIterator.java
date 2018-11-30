821263682223a0c7dc2364f7751982bae99a4e7f
fabianfroding <fabian.froding@gmail.com>
2018-11-30 22:25:12 +0100

    Friday night update yay

--- source-code-modeler/src/sourcecodemodeler/controller/XMLIterator.java
+++ source-code-modeler/src/sourcecodemodeler/controller/XMLIterator.java
@@ -32,7 +32,7 @@ public class XMLIterator {
                 .replace(".xml", "")
                 .replace(".java", "")
         );
-        //setAttributes(name, xmlClass);
+        setAttributes(name, xmlClass);
         setMethods(name, xmlClass);
         //setRelationships();
         return xmlClass;
@@ -45,13 +45,17 @@ public class XMLIterator {
             DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
             Document doc = docBuilder.parse(new File(pathToXMLDirectory + xmlFileName));
 
-            NodeList nodeList = doc.getElementsByTagName("decl"); // Tag for attributes.
+            NodeList nodeList = doc.getElementsByTagName("decl_stmt"); // Tag for attributes.
             for (int i = 0; i < nodeList.getLength(); i++) {
                 Node node = nodeList.item(i);
-                node = removeAnnotations(node);
-                node = removeExceptions(node);
+                Node secondParent = node.getParentNode().getParentNode();
+                if (secondParent.getNodeName() == "class"/* && GetClassName(secondParent) == xmlClass.getName()*/) {
+                    node = prettyAttribute(node);
+                    String s = node.getTextContent();
 
-
+                    s = prettyString(s);
+                    xmlClass.addAttribute(s);
+                }
             }
         } catch (ParserConfigurationException | org.xml.sax.SAXException | IOException e) {
             e.printStackTrace();
@@ -87,6 +91,21 @@ public class XMLIterator {
     }
 
     //----- Remove Specific Tags -----//
+    private Node removeTag(Node node, String tag) {
+        NodeList childNodes = node.getChildNodes();
+        for (int i = 0; i < childNodes.getLength(); i++) {
+            Node childNode = childNodes.item(i);
+            String childNodeName = childNode.getNodeName();
+            // Check if this child should be removed by checking its name.
+            if (childNodeName.equalsIgnoreCase(tag)) {
+                childNodes.item(i).setTextContent("");
+            }
+            else {
+                removeTag(childNode, tag); // Recursively call this function to remove children.
+            }
+        }
+        return node;
+    }
     private Node removeAnnotations(Node node) {
         NodeList childNodes = node.getChildNodes();
         for (int i = 0; i < childNodes.getLength(); i++) {
@@ -108,13 +127,27 @@ public class XMLIterator {
         return node;
     }
 
+    private Node prettyAttribute(Node node) {
+        node = removeTag(node, "init");
+        node = removeTag(node, "comment");
+        return node;
+    }
     private String prettyString(String s) {
+        s = s.replaceAll("\\s\\s", " "); //remove double empty space
+        s = s.replaceAll("\\s,\\s+", ", "); //remove space before comma
+        s = s.replaceAll("\\s*;", ""); // remove semi-column,
+        if (!(s.contains("public") || s.contains("private") || s.contains("protected")))
+        {
+            s = "+ " + s.trim(); // set public as default visibility
+        }
+
         s = s.trim()
                 .replace("public", "+")
                 .replace("private", "-")
                 .replace("protected", "#")
                 .replace("void", "")
                 .replace("static", "")
+                .replace("final", "")
                 .replace("   ", " ")
                 .replace("  ", " ");
         return s;
